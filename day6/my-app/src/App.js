06e882d2e8f332cdec25769491d178d98d001ddc
siva100625 <sivasubramanian.v2023ece@sece.ac.ingamil.com>
2025-01-11 12:42:13 +0530

    l

---
+++ day6/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Header } from './assets/Header';
+function App() {
+  return (
+    <div>
+      <h1>Hi bro</h1>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day6/my-app/src/assets/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <h1>Welcome to my page</h1>
+    </div>
+  )
+}
